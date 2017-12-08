8141715ea30e1f6f30b9cddd565a12dfd3defb70
diegomhamilton <diegohamilton26@gmail.com>
2017-12-08 19:34:04 -0300

    Add files via upload

--- quizVHDL.vhd
+++ quizVHDL.vhd
@@ -1,109 +1,208 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 
 entity quizVHDL is
 	port (
 		clk		: in std_logic;
-		--radd		: in std_logic_vector(1 downto 0);
-		--set	: in std_logic;
-		--name	: in std_logic_vector(29 downto 0);
-		--tempo : in std_logic_vector(7 downto 0);
-		--data_out	: out std_logic_vector(59 downto 0);
-		button : out std_logic_vector(3 downto 0);
+		button : in std_logic_vector(3 downto 0);
 		rw, rs, e : out std_logic;
-		lcd_data : out std_logic_vector(7 downto 0)
+		lcd_data : out std_logic_vector(7 downto 0);
+		--FLASH
+		CE  : out std_logic;
+		OE  : out std_logic;
+		address_memory : out std_logic_vector(22 downto 0);
+		flash_data : in std_logic_vector(7 downto 0);
+		--DEBUG
+		deb_start : out std_logic;
+		deb_turno : out std_logic;
+		deb_b_edge : out std_logic_vector(3 downto 0);
+		deb_acertos: out std_logic_vector(2 downto 0);
+		state : out std_logic_vector(2 downto 0)
 		);
 end entity;
 
 architecture rtl of quizVHDL is
-	type state is (init, select_question, check_answer, write_name, ranking1, ranking2);
-	state ss;
-	signal we			: std_logic;
-	signal wadd			: std_logic_vector(1 downto 0);
-	signal data_in		: std_logic_vector(59 downto 0);
-	signal nwena, nwup, nwdown, nwsel, nwrstn, nwfinished : std_logic;
-	signal lcreset_n, lclcd_enable, lcbusy: std_logic;
-	signal nwlcd_bus, lclcd_bus : std_logic_vector(9 downto 0);
-		
+	signal lcd_enable : std_logic;
+	signal lcd_bus    : std_logic_Vector(9 downto 0);
+	signal lcd_busy   : std_logic;
+	-- Buttons
+	signal b_deb		: std_logic_vector(3 downto 0);
+	signal b_edge		: std_logic_vector(3 downto 0);
+	-- Write questions
+	signal rfrw, rfrs, rfe : std_logic;
+	signal rflcd_data      : std_logic_Vector(7 downto 0);
+	signal fim_turno 		  : std_logic;
+	signal gabarito	  	  : std_logic_vector(3 downto 0);
+	signal rfrst		     : std_logic;
+	-- Turno
+	signal acerto		  : std_logic;
+	signal start		  : std_logic;
+	signal tfim_turno   : std_logic;
+	signal tempo		  : integer range 0 to 30;
+	-- Integracao
+	signal no_acertos	  : std_logic_vector(2 downto 0);
+	
+	type state_type is (init, Q1, Q2, Q3, Q4, Q5, ranking);
+	signal ss : state_type;
+	
 begin
-	controller: entity work.lcd_controller port map (
-		clk  => clk,
-		reset_n => lcreset_n,
-		lcd_enable => lclcd_enable,
-		lcd_bus => lclcd_bus,
-		busy => lcbusy,
-		rw => rw,
-		rs => rs,
-		e => e,
-		lcd_data => lcd_data
-	);
+--	dut: entity work.lcd_controller
+--		port map(clk => clk, reset_n => '1', lcd_enable => lcd_enable, lcd_bus => lcd_bus, 
+--					busy => lcd_busy, rw => rw, rs => rs, e => e, lcd_data => lcd_data);
+--	
+	deb_b0: entity work.debounce
+		port map (clk => clk, button => button(0), result => b_deb(0));
+	deb_b1: entity work.debounce
+		port map (clk => clk, button => button(1), result => b_deb(1));
+	deb_b2: entity work.debounce
+		port map (clk => clk, button => button(2), result => b_deb(2));
+	deb_b3: entity work.debounce
+		port map (clk => clk, button => button(3), result => b_deb(3));
 	
-	ram: entity work.ramking port map (
-		clk => clk, 
-		we => we, 
-		wadd => wadd, 
-		radd => radd, 
-		data_in => data_in,
-		data_out => data_out
-	);
+	edge_b0: entity work.edge_detector 
+		port map (clk => clk, signal_in => not(b_deb(0)), output => b_edge(0));
+	edge_b1: entity work.edge_detector 
+		port map (clk => clk, signal_in => not(b_deb(1)), output => b_edge(1));
+	edge_b2: entity work.edge_detector 
+		port map (clk => clk, signal_in => not(b_deb(2)), output => b_edge(2));
+	edge_b3: entity work.edge_detector
+		port map (clk => clk, signal_in => not(b_deb(3)), output => b_edge(3));
 	
-	ranks: entity work.ranking port map (
-		clk => clk,
-		set => set,
-		name => name,
-		tempo => tempo,
-		clear => '0',
-		data_ram => data_in,
-		addr_ram => wadd,
-		we_ram => we
-	);
-	
-	name_writer: entity work.name_writer port map (
-		up => nwup,
-		down => nwdown,
-		sel => nwsel,
-		clk => clk,
-		ena => nwena,
-		rstn => nwrstn,
-		finished => nwfinished;
-		lcd_bus => nwlcd_bus
-	);
-	
-	process(clk, rst)
-	begin
-		if (rst) then
+	write_question: entity work.read_flash
+		port map(clk => clk, reset => rfrst, turno => fim_turno, gabarito => gabarito,
+					rw => rfrw, rs => rfrs, e => rfe, lcd_data => rflcd_data,
+					CE => CE, OE => OE, address_memory => address_memory, data => flash_data);
+	jogada: entity work.turno
+		port map(clk => clk, button_edge => b_edge, gabarito => gabarito, fim_turno => tfim_turno,
+		acerto => acerto, start => start, tempo => tempo);
 		
-		end if;
-		if (rising_edge(clk)) then
-			case ss is 
+	process(ss)
+	begin
+		case ss is
+			when init =>
+				rw <= rfrw;
+				rs <= rfrs;
+				e 	<= rfe;
+				lcd_data <= rflcd_data;
+				
+				fim_turno <= b_edge(0) or b_edge(1) or b_edge(2) or b_edge(3);
+				state <= "000";
+			when Q1 =>
+				rw <= rfrw;
+				rs <= rfrs;
+				e 	<= rfe;
+				lcd_data <= rflcd_data;
+				
+				fim_turno <= tfim_turno;
+				state <= "001";
+			when Q2 =>
+				rw <= rfrw;
+				rs <= rfrs;
+				e 	<= rfe;
+				lcd_data <= rflcd_data;
+				
+				fim_turno <= tfim_turno;
+				state <= "010";
+			when Q3 =>
+				rw <= rfrw;
+				rs <= rfrs;
+				e 	<= rfe;
+				lcd_data <= rflcd_data;
+				
+				fim_turno <= tfim_turno;
+				
+				state <= "011";
+			when Q4 =>
+				rw <= rfrw;
+				rs <= rfrs;
+				e 	<= rfe;
+				lcd_data <= rflcd_data;
+				
+				fim_turno <= tfim_turno;
+				
+				state <= "100";
+			when Q5 =>
+				rw <= rfrw;
+				rs <= rfrs;
+				e 	<= rfe;
+				lcd_data <= rflcd_data;
+				
+				fim_turno <= tfim_turno;
+				
+				state <= "101";
+			when ranking =>
+				state <= "110";
+		end case;
+	end process;
+	
+	process(clk)
+	begin
+		if(rising_edge(clk)) then
+			case ss is
 				when init =>
-					if (lcd_busy = '0' and lcd_enable = '0') then
-						ss <= select_question;
+					if ((b_edge /= "0000") and (b_edge /= "1111")) then
+						start <= '1';
+						ss <= Q1;
 					else
-						lcd_enable <= '0';
+						start <= '0';
+						ss <= init;
 					end if;
-				when select_question =>
-				
-				when check_answer =>
-				
-				when write_name =>
-					nwena <= '1';
-					nwup <= button[0];
-					nwdown <= button[1];
-					nwsel <= button[3];
-					
-					lclcd_bus <= nwlcd_bus;
-					lclcd_enable <= nwlcd_enable;
-					
-					if (nwfinished = '1') then]
-						ss <= ranking1;
+				when Q1 =>
+					if (fim_turno ='1')  then
+						start <= '1';
+						ss <= Q2;
+					else
+						start <= '0';
+						ss <= Q1;
 					end if;
-					
-				when ranking1 =>
-				
-				when ranking2 =>
+				when Q2 =>
+					if (fim_turno ='1')  then
+						start <= '1';
+						ss <= Q3;
+					else
+						start <= '0';
+						ss <= Q2;
+					end if;
+				when Q3 =>
+					if (fim_turno ='1')  then
+						start <= '1';
+						ss <= Q4;
+					else
+						start <= '0';
+						ss <= Q3;
+					end if;
+				when Q4 =>
+					if (fim_turno ='1')  then
+						start <= '1';
+						ss <= Q5;
+					else
+						start <= '0';
+						ss <= Q4;
+					end if;
+				when Q5 =>
+					if (fim_turno ='1')  then
+						ss <= ranking;
+					else
+						start <= '0';
+						ss <= Q5;
+					end if;
+				when ranking =>
+					if (b_edge(0) = '1') then
+						ss <= init;
+					end if;
 			end case;
 		end if;
+		
+		if(acerto = '1') then
+			no_acertos <= no_acertos + '1';
+		end if;
 	end process;
+	
+	deb_start <= start;
+	deb_turno <= fim_turno;
+	deb_b_edge <= b_edge;
+	deb_acertos <= no_acertos;
 end architecture;
--- turno.vhd
+++ turno.vhd
@@ -4,14 +4,12 @@ use ieee.numeric_std.all;
 
 entity turno is
 	port ( clk		 : in std_logic;
-		button1		 :	in std_logic;
-		button2		 :	in std_logic;
-		button3		 :	in std_logic;
-		button4		 :	in std_logic;
-		gabarito		 :	in std_logic_vector(3 downto 0) ;
+		button_edge	 :	in std_logic_vector(3 downto 0);
+		gabarito		 :	in std_logic_vector(3 downto 0);
 		fim_turno	 : out std_logic;
 		acerto		 : out std_logic;
-		tempo			 : out std_logic_vector(4 downto 0) 
+		start			 : in std_logic;
+		tempo			 : out integer range 0 to 30
 	);
 end entity;
 
@@ -19,43 +17,45 @@ architecture comparation of turno is
 	type state is (init, wait_res, comp, result);
 	signal ss : state;
 	signal acerto_aux	: std_logic;
-	signal pressed : std_logic_vector(3 downto 0);
+	signal start_edge : std_logic;
+	signal turno	: std_logic;
 	component edge_detector is
       port ( clk               : in  STD_LOGIC;
                  signal_in   : in  STD_LOGIC;
                  output         : out  STD_LOGIC);
 	end component;
 begin
-	b1: edge_detector
-			port map (clk => clk, signal_in => not(button1), output => pressed(0));
-	b2: edge_detector
-			port map (clk => clk, signal_in => not(button2), output => pressed(1));
-	b3: edge_detector
-			port map (clk => clk, signal_in => not(button3), output => pressed(2));
-	b4: edge_detector
-			port map (clk => clk, signal_in => not(button4), output => pressed(3));
+	beginning: edge_detector
+			port map (clk => clk, signal_in => start, output => start_edge);
+	edge:	edge_detector PORT MAP (
+          clk => clk, signal_in => turno, output => fim_turno
+        );
 	
 	process (clk)
 	variable resposta: std_logic_vector(3 downto 0);
-	variable c	: integer range 0 to 1; --50000000
-	variable k	: integer range 0 to 5; --30
+	variable c	: integer range 0 to 50000000;
+	variable k	: integer range 0 to 30;
 	begin
+		tempo <= k;
+		
 		if (clk'event and clk='1') then
 			c  := c + 1;
 			
 			case ss is
 				when init=>
-					fim_turno <= '0';
-					ss<= wait_res;
+					turno <= '0';
+					acerto_aux <= '0';
+					if (start_edge = '1') then 
+						ss<= wait_res;
+					end if;
 				when wait_res => -- etapa de espera da resposta do jogador
-					fim_turno <= '0';
-					if (k = 5) then -- tempo esgotado = erro (k = 30)
+					turno <= '0';
+					if (k = 30) then
 						resposta := "0000";
 						ss <= result;
-					elsif (pressed /= "0000") then-- botão pressionado
-						resposta := pressed; -- para comparação posterior
+					elsif (button_edge /= "0000") then-- botão pressionado
+						resposta := button_edge; -- para comparação posterior
 						ss <= comp;
-						tempo <= std_logic_vector(to_unsigned(k, 5));
 					else									
 						if (c = 1) then -- c=50000000
 							k := k + 1;
@@ -64,7 +64,7 @@ begin
 						ss<= wait_res; -- continua neste estado
 					end if;
 				when comp => --etapa de verificação da resposta do jogador
-					fim_turno <= '0';
+					turno <= '0';
 					
 					if (gabarito = resposta) then
 						acerto_aux <= '1';
@@ -74,16 +74,14 @@ begin
 					
 					ss <= result;
 				when result=>
-					if (acerto_aux = '1') then
-						-- print acertou
-					else
-						-- print errou
-					end if;
+					turno <= '1';
 					
-					fim_turno <= '1';
+					if (start_edge = '1') then 
+						ss<= wait_res;
+					end if;
 			end case;
 		end if;
 	end process;
-		
+
 	acerto <= acerto_aux;
 	end architecture;
